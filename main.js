8ccdac539a2a9a29195b471d3371460b22c57d46
Roberto Gongora <robertogongora@hey.com>
2021-11-25 18:10:09 +0100

    Implemented input

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const $image = document.getElementById('image');
-// const $seconds = document.getElementById('seconds');
+const $seconds = document.getElementById('seconds');
 
 let images = [
     'https://cdn.shopify.com/s/files/1/0512/8444/3315/products/3757_150x.jpg?v=1617880643',
@@ -9,9 +9,16 @@ let images = [
     'https://cdn.shopify.com/s/files/1/0512/8444/3315/products/3764_150x.jpg?v=1617880644',
 ];
 
-setInterval(() => {
+let seconds = 5000;
+
+let runner = () => {
     $image.style.backgroundImage = `url("${images[getRandom(4)]}")`;
-}, 5000);
+    seconds = ($seconds.value || 5) * 1000;
+
+    setTimeout(runner, seconds);
+};
+
+setTimeout(runner, seconds);
 
 function getRandom(max) {
     return Math.floor(Math.random() * max);
