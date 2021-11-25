51a793d604fec1d43d38b2ae8a53ddba24ce5092
Roberto Gongora <robertogongora@hey.com>
2021-11-25 17:56:43 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const $image = document.getElementById('image');
+// const $seconds = document.getElementById('seconds');
+
+let images = [
+    'https://cdn.shopify.com/s/files/1/0512/8444/3315/products/3757_150x.jpg?v=1617880643',
+    'https://cdn.shopify.com/s/files/1/0512/8444/3315/products/3759_150x.jpg?v=1617880644',
+    'https://cdn.shopify.com/s/files/1/0512/8444/3315/products/3760_150x.jpg?v=1617880644',
+    'https://cdn.shopify.com/s/files/1/0512/8444/3315/products/3762_150x.jpg?v=1617880644',
+    'https://cdn.shopify.com/s/files/1/0512/8444/3315/products/3764_150x.jpg?v=1617880644',
+];
+
+setInterval(() => {
+    $image.style.backgroundImage = `url("${images[getRandom(4)]}")`;
+}, 5000);
+
+function getRandom(max) {
+    return Math.floor(Math.random() * max);
+}
